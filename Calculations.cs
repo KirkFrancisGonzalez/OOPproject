dc7cdf925ccb5c2ecbf9512566b55db5ec78b515
KirkFrancisGonzalez <45276166+KirkFrancisGonzalez@users.noreply.github.com>
2018-11-23 09:29:24 +0800

    Added Files
    Added Calculations.cs, Person.cs, Product.cs

---
+++ Calculations.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_project
+{
+    class Calculations
+    {
+        public int CalculateAge(string birthDate)
+        {
+            return 0;
+        }
+        
+        decimal CalculateInterest( decimal prinicipalAmount, decimal monthlyRate)
+        {
+            return 0;
+        }
+
+        decimal CalculateAccruedAmount(decimal principalAmount, decimal monthlyRate, string date)
+        {
+            return 0;
+        }
+
+    }
+}
---
+++ Person.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP_project
+{
+    class Person
+    {
+        public string FirstName;
+        public string MiddleInitial;
+        public string LastName;
+        public string Birthdate;
+        public string Address;
+
+        public Person()
+        {
+           
+        }
+
+        public string FullName()
+        {
+            return null;
+        }
+
+        public int GetAge()
+        {
+            return 0;
+        }
+    }
+}
