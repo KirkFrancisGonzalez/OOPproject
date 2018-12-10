d046445577cad4bc77e1537220d5c6755d442cdf
KirkFrancisGonzalez <45276166+KirkFrancisGonzalez@users.noreply.github.com>
2018-12-10 09:27:27 +0800

    Added TransactionWindow, JewelryWindow, Deleted old Files
    No code yet for transactions, only windows

---
+++ OOP_PROJECT(tinood)/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OOP_PROJECT_tinood_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AddTransaction trans = new AddTransaction();
+            trans.Show();
+            this.Close();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            AddJewelry jew = new AddJewelry();
+            jew.Show();
+            this.Close();
+        }
+    }
+}
