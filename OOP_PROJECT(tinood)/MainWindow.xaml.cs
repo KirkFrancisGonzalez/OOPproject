b7d6f348fd0bca10dc0d92b53c1da783b2f196a2
KirkFrancisGonzalez <45276166+KirkFrancisGonzalez@users.noreply.github.com>
2018-12-17 09:24:11 +0800

    Rearranged

--- OOP_PROJECT(tinood)/MainWindow.xaml.cs
+++ OOP_PROJECT(tinood)/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace OOP_PROJECT_tinood_
 {
@@ -36,6 +24,11 @@ namespace OOP_PROJECT_tinood_
         {
             AddJewelry jew = new AddJewelry();
             jew.Show();
+            this.Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
             this.Close();
         }
     }
